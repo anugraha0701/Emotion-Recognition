127545e6ae63cab111ff8c7a97189b452f03f3c5
poorni-1809 <poornimaa182001@gmail.com>
2022-04-22 12:13:38 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response
+from camera import Video
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/emotion')
+def emotion_page():
+    return render_template('emotion.html')
+
+
+@app.route('/no')
+def no_page():
+    return render_template('no.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+       b'Content-Type:  image/jpeg\r\n\r\n' + frame +
+         b'\r\n\r\n')
+
+@app.route('/video')
+
+def video():
+    return Response(gen(Video()),
+    mimetype = 'multipart/x-mixed-replace; boundary=frame')
+    
+
+app.run(debug = True)
